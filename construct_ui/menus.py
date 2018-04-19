0b15728f8010b53c19d5be62d2406f109da401f7
Dan Bradham <danielbradham@gmail.com>
2018-04-19 13:09:01 -0400

    Decouple ActionMenu and create_action_menu fn to support Maya

--- construct_ui/menus.py
+++ construct_ui/menus.py
@@ -15,50 +15,33 @@ class ActionMenu(QtWidgets.QMenu):
         self.aboutToShow.connect(self.before_show)
 
     def before_show(self):
-        self.clear()
-        self.create()
-
-    def create(self, groups=None, parent=None):
-
-        if not groups:
-            groups = group_actions(construct.actions.collect())
-            parent = self
-
-        for group, actions in groups.items():
-            if group == '':
-                for action in actions:
-                    qaction = self._create_action(action, parent)
-                    parent.addAction(qaction)
-            else:
-                submenu = QtWidgets.QMenu(group.title(), parent)
-                parent.addMenu(submenu)
-                self.create(actions, submenu)
-
-    def _create_action(self, action, parent):
-        '''Creates a QAction from a Construct Action'''
-
-        def _run_action():
-            action().run()
-
-        qaction = QtWidgets.QAction(action.label, parent)
-        qaction.triggered.connect(_run_action)
-        return qaction
+        create_action_menu(parent=self)
 
 
+def create_action_menu(groups=None, parent=None):
+    '''Decoupled from ActionMenu class so we can use it with Maya directly'''
 
-def main():
-    import sys
-    import construct
-    construct.init()
-    construct.set_context_from_path(
-        'C:/Users/danie/projects/faux/assets/product/coffee/model/work/maya'
-    )
-    app = QtWidgets.QApplication([])
-    win = QtWidgets.QMainWindow()
-    bar = win.menuBar()
-    bar.addMenu(ActionMenu('Actions', win))
-    win.show()
-    sys.exit(app.exec_())
+    if groups is None:
+        groups = group_actions(construct.actions.collect())
+        parent.clear()
 
-if __name__ == '__main__':
-    main()
+    for group, actions in groups.items():
+        if group == '':
+            for action in actions:
+                qaction = create_action_menu_item(action, parent)
+                parent.addAction(qaction)
+        else:
+            submenu = QtWidgets.QMenu(group.title(), parent)
+            parent.addMenu(submenu)
+            create_action_menu(actions, submenu)
+
+
+def create_action_menu_item(action, parent):
+    '''Creates a QAction from a Construct Action'''
+
+    def run_action():
+        action().run()
+
+    qaction = QtWidgets.QAction(action.label, parent)
+    qaction.triggered.connect(run_action)
+    return qaction
